febd7209932f91df5c3e02047d5638b25c02a999
z3budah <714606775@qq.com>
2023-04-10 06:04:49 +0800

    Change the interpolation method. The previous method was looking for the nearest valid frames. Now it uses all the valid frames to do Akima interpolation, and the invalid frame can get the point value from the interpolation function.

--- kpredraw.py
+++ kpredraw.py
@@ -1,99 +1,84 @@
 # -*- coding: utf-8 -*-
+"""kpredraw.py
+Redraw the joint keypoints.  Fixed low confidence/out of image joint keypoint by Akima interpolation.
+"""
 import os
 import re
-
+import json
 import numpy as np
 import cv2
 import logging
 import multiprocessing as mp
 from tqdm import tqdm
 
-"""kpredraw.py
-Redraw the joint keypoints.  Fixed low confidence/out of image joint keypoint by interpolation between frames.
-"""
+from scipy.interpolate import Akima1DInterpolator
+from scipy.interpolate import interp1d
+
+
 
 # list of json file paths
-keypoint_dir = "../action/keypoint"
-keypoints = sorted(
+json_dir="../action/annotation"
+json_files = sorted(
     [
-        os.path.join(keypoint_dir, fname)
-        for fname in os.listdir(keypoint_dir)
-        if fname.endswith(".npy")
+        os.path.join(json_dir, fname)
+        for fname in os.listdir(json_dir)
+        if fname.endswith(".json")
     ]
 )
-
-# load the background  image
-image = cv2.imread('black.jpg')
-height, width, _ = image.shape
 
 # joint keypoint confidence thereshold
 c_threshold = 0.1
 
 
 # get valid point from adjacent frames
-def fix_lost_keypoint(keypoints):
+def fix_lost_keypoint(keypoints, image):
     # select frame with average confidence > 0.1, discard low confidence frames
     valid_frame = np.mean(keypoints, axis=1)[:, 2] > c_threshold
     new_keypoints = keypoints[valid_frame]
     l = len(new_keypoints)
+    height, width, _= image.shape
     # for each point check all the frame
-    for p in range(0, 18):
+    for p in range(0, 14):
         logging.info("processsing point {}".format(p))
         arr_p = np.array([new_keypoints[f][p] for f in range(0, l)])
+
         # valid point: 0<=x<=width, 0<=y<=height, 0<confidence<1.0
-        valid = np.logical_and.reduce(
-            [arr_p.T[0] <= width, arr_p.T[1] <= height, arr_p.T[2] > c_threshold, arr_p.T[2] < 1.0])
+        valid = np.logical_and.reduce([arr_p.T[0] <= width, arr_p.T[1] <= height, arr_p.T[2] > c_threshold, arr_p.T[2] < 1.0])
+        if not valid.any():
+            logging.error("all points are invalid")
+            continue
+
+        # Akima interpolation
+        # extend the range at the beginning and end of the valid array, at frame -1 and l : (x,y) take the nearest valid value
+        t = np.array(range(0, l))
+        t = t[valid]
+        t = np.insert(t, 0, -1)
+        t = np.append(t, l)
+
+
+        ctrl_points = arr_p.T[:2].T[valid]
+        ctrl_points = np.insert(ctrl_points, 0, ctrl_points[0],axis=0)
+        ctrl_points = np.append(ctrl_points, [ctrl_points[-1]], axis=0)
+
+        inter_func = Akima1DInterpolator(t,ctrl_points)
+
         # get invalid frame index
         fail_f = np.where(valid == False)[0]
-        if len(fail_f) == 0:
-            logging.info("all the frames are qualified")
-            continue
-        elif len(fail_f) == l:
-            # all the frames are unqualified
-            logging.error("all the frames are unqualified")
-            continue
-        else:
-            logging.info("all the invalid frames:{}".format(fail_f))
-            for f_idx in fail_f:
-                logging.info("invalid: frame {} point {}".format(f_idx, p))
-                # previous valid frame
-                f_prev = f_idx - 1
-                while f_prev >= 0:
-                    if valid[f_prev]:
-                        break
-                    else:
-                        f_prev = f_prev - 1
+        inter_points = inter_func(fail_f)
 
-                # next valid frame
-                f_next = f_idx + 1
-
-                while f_next < l:
-                    if valid[f_next]:
-                        break
-                    else:
-                        f_next = f_next + 1
-
-                logging.info(f_idx, "old point:", arr_p[f_idx])
-
-                if f_prev < 0:
-                    arr_p[f_idx] = arr_p[f_next]
-                elif f_next >= l:
-                    arr_p[f_idx] = arr_p[f_prev]
-                else:
-                    logging.info("prev frame {} and next frame {}:".format(f_prev, f_next), valid[f_prev],
-                                 valid[f_next])
-
-                    # interpolation
-                    inter = f_next - f_prev + 1
-                    arr_p[f_idx][0] = (f_idx - f_prev) / inter * arr_p[f_prev][0] + (f_next - f_idx) / inter * \
-                                      arr_p[f_next][0]
-                    arr_p[f_idx][1] = (f_idx - f_prev) / inter * arr_p[f_prev][1] + (f_next - f_idx) / inter * \
-                                      arr_p[f_next][1]
-                    arr_p[f_idx][2] = c_threshold + 0.01
-
-                logging.info(f_idx, "new point: [{:.2f},{:.2f}]".format(arr_p[f_idx][0], arr_p[f_idx][1]))
-
+        nan_f = []
+        for i, f_idx in enumerate(fail_f):
+            arr_p[f_idx][0] = inter_points[i][0]
+            arr_p[f_idx][1] = inter_points[i][1]
+            arr_p[f_idx][2] = c_threshold + 0.01
+            #logging.info(f_idx, "new point: [{:.2f},{:.2f}]".format(inter_points[i][0], inter_points[i][1]))
+            if np.isnan(inter_points[i][0]) or np.isnan(inter_points[i][1]):
+                nan_f.append(f_idx)
+                logging.error(p,f_idx, "new point: [{:.2f},{:.2f}]".format(inter_points[i][0], inter_points[i][1]))
+            else:
+                valid[f_idx] = True
                 new_keypoints[f_idx][p] = arr_p[f_idx]
+
 
     return new_keypoints
 
@@ -109,7 +94,8 @@ pose_colors = [(0, 100, 255), (0, 100, 255), (0, 255, 255), (0, 100, 255), (0, 2
 
 
 # redraw the joint keypoints
-def redraw(pose_keypoints_2d, fixed=True):
+def redraw(pose_keypoints_2d, image, title, fixed=True):
+    title = (title + "Fixed Pose Keypoints" if fixed else "Original Pose Keypoints")
     # Loop over each frame and draw the keypoints for each frame
     for idx, frame in enumerate(pose_keypoints_2d):
 
@@ -125,8 +111,9 @@ def redraw(pose_keypoints_2d, fixed=True):
                 except Exception as e:
                     logging.error("bad point frame {}: point{}:[{:.2f},{:.2f}],point {}:[{:.2f},{:.2f}]".format(
                         idx, pair[0], x1, y1, pair[1], x2, y2))
+
         # Display the resulting image
-        title = "Fixed Pose Keypoints" if fixed else "Original Pose Keypoints"
+        cv2.putText(image, "frame:{}".format(idx), (0, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 3)
         cv2.imshow(title, image)
         if cv2.waitKey(100) & 0xFF == ord('q'):
             break
@@ -137,9 +124,9 @@ def redraw(pose_keypoints_2d, fixed=True):
     cv2.destroyAllWindows()
 
 
-def run_multiprocess(original, fixed):
-    p1 = mp.Process(target=redraw, args=(fixed,))
-    p2 = mp.Process(target=redraw, args=(original, False,))
+def run_multiprocess(original, fixed, img, title):
+    p1 = mp.Process(target=redraw, args=(fixed,img,title))
+    p2 = mp.Process(target=redraw, args=(original,img,title, False,))
 
     p1.start()
     p2.start()
@@ -149,10 +136,39 @@ def run_multiprocess(original, fixed):
 
 
 if __name__ == '__main__':
+    # for keypoint_path in tqdm(keypoints):
+    #     # load the pose keypoints from the numpy array
+    #     pose_keypoints_2d = np.load(keypoint_path)
+    #     new_keypoints = fix_lost_keypoint(pose_keypoints_2d)
+    #     run_multiprocess(pose_keypoints_2d, new_keypoints)
+    #     np.save(re.sub("keypoint", "new_keypoint", keypoint_path),new_keypoints)
+    try:
+        with open(json_files[0], "r") as f:
+            data = json.load(f)
+    except Exception as e:
+        logging.error("Failed to load annotation file:{}".format(e))
 
-    for keypoint_path in tqdm(keypoints):
-        # load the pose keypoints from the numpy array
-        pose_keypoints_2d = np.load(keypoint_path)
-        new_keypoints = fix_lost_keypoint(pose_keypoints_2d)
-        run_multiprocess(pose_keypoints_2d, new_keypoints)
-        np.save(re.sub("keypoint", "new_keypoint", keypoint_path),new_keypoints)
+    # extract relevant information
+    width = data["width"]
+    height = data["height"]
+    fps = data["fps"]
+    # create the background  image
+    img = np.zeros((height, width, 3), np.uint8)
+
+    video_id = list(data.keys())[0]
+    for annotation in data.get(video_id, []):
+        if annotation["keypoint"] is None:
+            continue
+        if os.path.exists(re.sub("keypoint", "new_keypoint", annotation["keypoint"])):
+            logging.error("Keypoints already fixed.")
+            continue
+        print(annotation["keypoint"])
+        keypoints = np.load(annotation["keypoint"])
+        new_keypoints = fix_lost_keypoint(keypoints,img)
+        print(new_keypoints[:,0:14,0:2].shape)
+        # redraw(new_keypoints, img, annotation["label"])
+        np.save(re.sub("keypoint", "new_keypoint", annotation["keypoint"]), new_keypoints[:,0:14,0:2])
+        # run_multiprocess(keypoints, new_keypoints, img, annotation["label"])
+
+
+
